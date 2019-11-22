a4244eeea5bce92f110d6d80d9e03a442314a61f
srimal_n <srimal@pickme.lk>
2019-11-22 18:39:45 +0530

    Added unit tests and benchmarks

--- session2/9.crud-persistent/pkg/models/person.go
+++ session2/9.crud-persistent/pkg/models/person.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 type Person struct {
 	ID          int    `json:"id,omitempty"`
 	Firstname   string `json:"firstname,omitempty"`
@@ -11,4 +13,9 @@ type ContactInfo struct {
 	City     string `json:"city,omitempty"`
 	AreaCode string `json:"areacode,omitempty"`
 	Phone    int    `json:"phone,omitempty"`
+}
+
+// GetDialNumber retrns the dialing number
+func (p *Person) GetDialNumber() string {
+	return p.AreaCode + fmt.Sprintf("%d", p.Phone)
 }
